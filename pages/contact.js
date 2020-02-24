9fed16a302ee50580d838f52f0ec9deb790de9aa
ebrima <ebrima.jobe92@gmail.com>
2020-02-24 02:20:02 -0800

    finer tuned contact page

--- pages/contact.js
+++ pages/contact.js
@@ -11,7 +11,7 @@ const data = {
 const Home = () => (
     <div>
         <Wrapper data={data}>
-            <article class="helvetica pt5">
+            <article class="helvetica pt6">
                 <div className='measure-wide mt3 mb3'>
                     <section class="cf w-100 pa2-ns">
                         <span className={`${styles.section}`}>
@@ -20,6 +20,13 @@ const Home = () => (
                     </section>
                 </div>
                 <div class="serif ph2 ph4-m ph5-l pb5">
+                <div class="f3 fw7 black lh-title pa2 pb3">
+                🚀 <a href="mailto:blachaz3@aol.com" class={`${styles.link}`}>
+                 blachaz3@aol.com </a>
+                </div>
+               <h4 className=" f4-m f4-l mt0 mb4 measure fw6">
+                    Use the form below to tell us about your marketing and event programming needs. Our general response time is one business day.
+                    </h4>
                     {/* <div class="db dtc-ns v-mid-ns w-100 mw7 w5-ns"> */}
                         <div class="measure ph2  ">
                             <label for="name" class="f6 b db mb2">Name </label>
@@ -61,9 +68,14 @@ const Home = () => (
                             <label for="name" class="f6 b db mb2 bg-transparent hover-bg-black hover-white">More info</label>
                             <textarea id="comment" name="comment" class="db border-box hover-black w-100 measure ba b--black-20 pa2 br2 mb2" aria-describedby="comment-desc"></textarea>
                         </div>
+                        <div class="measure ph2 pt4">
+                            <input class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f4" type="submit" value="Submit"/>
+                        </div>
+
                     {/* </div> */}
                 </div>
             </article>
+            
         </Wrapper>
     </div>
     )
