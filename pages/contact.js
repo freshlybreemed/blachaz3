b468d8957f64d5d7c530a178a372fdfae19bee9a
ebrima <ebrima.jobe92@gmail.com>
2020-02-24 20:33:25 -0800

    contact component integration

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,6 @@
 import { styles } from '../constants/styles';
 import React from 'react';
-// import SignUpForm from '../components/SignUpForm';
+import ContactForm from '../components/ContactForm';
 import Wrapper from '../components/Wrapper';
 
 const data = {
@@ -27,52 +27,7 @@ const Home = () => (
                <h4 className=" f4-m f4-l mt0 mb4 measure fw6">
                     Use the form below to tell us about your marketing and event programming needs. Our general response time is one business day.
                     </h4>
-                    {/* <div class="db dtc-ns v-mid-ns w-100 mw7 w5-ns"> */}
-                        <div class="measure ph2  ">
-                            <label for="name" class="f6 b db mb2">Name </label>
-                            <input id="name" class="input-reset ba b--black-20 pa2 mb2 db w-100 bg-transparent hover-bg-black hover-white" type="text" aria-describedby="name-desc"/>
-                        </div>
-                        <div class="measure ph2">
-                            <label for="name" class="f6 b db mb2">Email </label>
-                            <input id="name" class="input-reset ba b--black-20 pa2 mb2 db w-100 bg-transparent hover-bg-black hover-white" type="text" aria-describedby="name-desc"/>
-                        </div>
-                        <div class="measure ph2">
-                            <label class="f6 b db mb2">How can Black Haze help you out?</label>
-                                <div class="flex items-center mb2">
-                                    <input class="mr2" type="checkbox" id="brandidentity" value="brandidentity"/>
-                                    <label for="brandidentity" class="lh-copy">Brand Identity</label>
-                                </div>
-                                <div class="flex items-center mb2">
-                                    <input class="mr2" type="checkbox" id="contentcreation" value="contentcreation"/>
-                                    <label for="contentcreation" class="lh-copy">Content Creation</label>
-                                </div>
-                                <div class="flex items-center mb2">
-                                    <input class="mr2" type="checkbox" id="socialmedia" value="socialmedia"/>
-                                    <label for="socialmedia" class="lh-copy">Social Media</label>
-                                </div>
-                                <div class="flex items-center mb2">
-                                    <input class="mr2" type="checkbox" id="consulting" value="consulting"/>
-                                    <label for="consulting" class="lh-copy">Consulting</label>
-                                </div>
-                                <div class="flex items-center mb2">
-                                    <input class="mr2" type="checkbox" id="marketing" value="marketing"/>
-                                    <label for="marketing" class="lh-copy">Marketing</label>
-                                </div>
-                                <div class="flex items-center mb2">
-                                    <input class="mr2 " type="checkbox" id="other" value="other"/>
-                                    <label for="proxy" class="lh-copy">Other</label>
-                                </div>
-
-                        </div>
-                        <div class="measure ph2">
-                            <label for="name" class="f6 b db mb2 bg-transparent hover-bg-black hover-white">More info</label>
-                            <textarea id="comment" name="comment" class="db border-box hover-black w-100 measure ba b--black-20 pa2 br2 mb2" aria-describedby="comment-desc"></textarea>
-                        </div>
-                        <div class="measure ph2 pt4">
-                            <input class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f4" type="submit" value="Submit"/>
-                        </div>
-
-                    {/* </div> */}
+                   <ContactForm />
                 </div>
             </article>
             
