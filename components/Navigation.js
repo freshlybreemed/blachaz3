ab5e1e38b4e87552244d4a601ebbccdec7a6e56d
ebrima <ebrima.jobe92@gmail.com>
2020-02-26 14:56:50 -0800

    fists up page

--- components/Navigation.js
+++ components/Navigation.js
@@ -9,30 +9,39 @@ const Navigation = () => {
   return (
     <header class='bg-white-90 fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l'>
       <nav class='f5 fw6  tracked'>
-        <a
-          class='link bg-black-90 pa2  white fw7 dim  dib mr3'
-          href='/'
-          title='Home'>
-          Home
-        </a>
-        <a
-          class='link bg-black-90 pa1  white  fw7 dim  dib mr3'
-          href='/about'
-          title='About'>
-          About
-        </a>
-        <a
-          class='link bg-black-90 pa2  white fw7 dim  dib mr3'
-          href='/projects'
-          title='Store'>
-          Fist Up Afros Out
-        </a>
-        <a
-          class='link  bg-black-90 pa1  white  fw7  dim  mr3'
-          href='/contact'
-          title='Contact'>
-          Contact
-        </a>
+        <Link href='/'>
+          <a class='link bg-black-90 pa2  white fw7 dim  dib mr3' title='Home'>
+            Home
+          </a>
+        </Link>
+        <Link href='/about'>
+          <a
+            class='link bg-black-90 pa1  white  fw7 dim  dib mr3'
+            title='About'>
+            About
+          </a>
+        </Link>
+        <Link href='/fists-up-afros-out'>
+          <a
+            class='link bg-black-90 pa2  white fw7 dim  dib mr3'
+            title='Fist Up Afros Out'>
+            Fist Up Afros Out
+          </a>
+        </Link>
+        <Link href='/projects'>
+          <a
+            class='link bg-black-90 pa1  white fw7 dim  dib mr3'
+            title='Fist Up Afros Out'>
+            Projects
+          </a>
+        </Link>
+        <Link href='/contact'>
+          <a
+            class='link  bg-black-90 pa2  white  fw7  dim  mr3'
+            title='Contact'>
+            Contact
+          </a>
+        </Link>
       </nav>
     </header>
   );
---
+++ pages/fists-up-afros-out.js
@@ -0,0 +1,109 @@
+import { styles } from '../constants/styles';
+import React from 'react';
+import Link from 'next/link';
+// import SignUpForm from '../components/SignUpForm';
+import Wrapper from '../components/Wrapper';
+
+const data = {
+  description: 'Fist Up Afros Out',
+  header: `Blachaz3`,
+};
+
+const Home = () => (
+  <div>
+    <Wrapper data={data}>
+      <article class='helvetica pt6'>
+        <div className='measure-wide mt3 mb3'>
+          <section class='cf w-100 pa2-ns'>
+            <span className={`${styles.section}`}>Fist Up Afros Out</span>
+          </section>
+        </div>
+        <div className='mw9 db center pa4 pt2-ns ph7-l'>
+          <img
+            className='w-80 center db f5 measure'
+            src='/static/img/fuao.png'
+          />
+          <h4 className=' f4-m f4-l mt0 mb4 measure fw6 pv4-l center'>
+            When you mix fashion and philanthropy, what do you get? A formula
+            that was created by mistake, but gaining it’s way to a household
+            brand. Hazel aims to promote UNITY through fashion, art and
+            community service.
+          </h4>
+          <h4 className=' f4-m f4-l mt0 mb2 measure fw6 pv4-l center'>
+            1. The Afro represents freedom, liberation, and an authenic form of
+            growth
+          </h4>
+          <h4 className=' f4-m f4-l mt0 mb2 measure fw6 pv4-l center'>
+            2. The Clenched Fist also known as the raised fist has been used for
+            centuries by various groups and cultures to symbolize unity.
+          </h4>
+          <h4 className=' f4-m f4-l mt0 mb2 measure fw6 pv4-l center'>
+            3. The Peace Sign besides loves, this is what the word needs
+          </h4>
+
+          <div className='mw9 db center pa2 pt2-ns'>
+            <img
+              className=' center db  w-100'
+              src='/static/img/48911613411_525c430a6e_o-2.jpg'
+            />
+          </div>
+          <div className='mw9 db center pa2 pt2-ns'>
+            <img
+              className=' center db  w-100'
+              src='/static/img/48911679631_b936776704_o-2.jpg'
+            />
+          </div>
+          <div className='mw9 db center pa2 pt2-ns'>
+            <img className=' center db  w-100' src='/static/img/DSC_0057.jpg' />
+          </div>
+          <div className='mw9 db center pa2 pt2-ns'>
+            <img className=' center db  w-100' src='/static/img/sofist1.jpg' />
+          </div>
+          <div className='mw9 db center pa2 pt2-ns'>
+            <img
+              className=' center db  w-100'
+              src='/static/img/sofist5 (2).jpg'
+            />
+          </div>
+          <div className='mw9 db center pa2 pt2-ns'>
+            <img
+              className=' center db  w-100'
+              src='/static/img/il_fullxfull.893805253_g43i.jpg'
+            />
+          </div>
+          <div className='mw9 db pr6 pa4 pt2-ns'>
+            <Link href='https://www.etsy.com/shop/Blachaz3?ref=simple-shop-header-name&listing_id=261679659'>
+              <a className=' center db  measure f5 fw7 dib no-underline black bg-animate hover-bg-black hover-white inline-flex pa3 ba bw1 border-box'>
+                Online Shop{' '}
+              </a>
+            </Link>
+          </div>
+          <div className='mw9 db pr6 pa4 pt2-ns'>
+            <iframe
+              width='560'
+              height='315'
+              src='https://www.youtube.com/embed/DKmhhjw-xhw'
+              frameborder='0'
+              allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+              allowfullscreen></iframe>
+          </div>
+        </div>
+        <div className='measure-wide mt3 mb3'>
+          <section class='cf w-100 pa2-ns'>
+            <span className={`${styles.section}`}>Videos</span>
+          </section>
+        </div>
+        <div className='mw9 w-90 bt center pa4 pt2-ns mh2'>
+          <iframe
+            width='560'
+            height='315'
+            src='https://www.youtube.com/embed/DKmhhjw-xhw'
+            frameborder='0'
+            allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+            allowfullscreen></iframe>
+        </div>
+      </article>
+    </Wrapper>
+  </div>
+);
+export default Home;
