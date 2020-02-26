f02f5c0ca338f4eee712ebf4e5917d0760e36793
ebrima <ebrima.jobe92@gmail.com>
2020-02-26 13:53:08 -0800

    fist up nav button

--- components/Navigation.js
+++ components/Navigation.js
@@ -7,12 +7,32 @@ const dev = process.env.NODE_ENV === 'development';
 
 const Navigation = () => {
   return (
-    <header class="bg-white-90 fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
-      <nav class="f5 fw6  tracked">
-        <a class="link bg-black-90 pa2  white fw7 dim  dib mr3" href="/" title="Home">Home</a>
-        <a class="link bg-black-90 pa1  white  fw7 dim  dib mr3" href="/about" title="About">About</a>
-        <a class="link bg-black-90 pa2  white fw7 dim  dib mr3" href="/projects" title="Store">Projects</a>
-        <a class="link  bg-black-90 pa1  white  fw7  dim  mr3" href="/contact" title="Contact">Contact</a>
+    <header class='bg-white-90 fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l'>
+      <nav class='f5 fw6  tracked'>
+        <a
+          class='link bg-black-90 pa2  white fw7 dim  dib mr3'
+          href='/'
+          title='Home'>
+          Home
+        </a>
+        <a
+          class='link bg-black-90 pa1  white  fw7 dim  dib mr3'
+          href='/about'
+          title='About'>
+          About
+        </a>
+        <a
+          class='link bg-black-90 pa2  white fw7 dim  dib mr3'
+          href='/projects'
+          title='Store'>
+          Fist Up Afros Out
+        </a>
+        <a
+          class='link  bg-black-90 pa1  white  fw7  dim  mr3'
+          href='/contact'
+          title='Contact'>
+          Contact
+        </a>
       </nav>
     </header>
   );
